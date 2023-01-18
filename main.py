548e2e933d1b12956fd6b451706ae155d8452f0a
Daniel Takeshi <martins.takeshi@gmail.com>
2023-01-18 02:36:27 +0000

    ADD new release

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! New Release 2.0!"
 # run the app.
 if __name__ == "__main__":
     app.run()
